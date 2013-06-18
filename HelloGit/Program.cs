3f57d0ee1862367ac82a619a48f987c9afabfbc3
Tony Martin <tony.martin@sage.com>
2013-06-18 11:21:47 -0600

    Added random fortune feature

--- HelloGit/Program.cs
+++ HelloGit/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace HelloGit
 {
     class Program
@@ -12,8 +13,21 @@ namespace HelloGit
         {
             Console.WriteLine("Hello, Git! You're wonderful!");
             Console.WriteLine("*** You have been modified for the better ***");
-            Console.WriteLine("Merge worked like a champ.");   // This is fine.
+            Console.WriteLine("\nYour fortune for today: {0}", GetFortune());
             Console.ReadKey();
+        }
+
+        public static string GetFortune()
+        {
+            Random rnd = new Random();
+            int whichFortune = rnd.Next(1, 3);
+            switch (whichFortune)
+            {
+                case 1: return "You will meet a tall, dark, stranger. When you do, RUN."; break;
+                case 2: return "You are still hungry. Order more Chinese food."; break;
+                case 3: return "The only danger is your own stupidity."; break;
+                default: return "No fortune for you!"; break;
+            }
         }
     }
 }
